32ced0b3364a68bcdcb533dc6447f7062f1c59f1
nivalkarDhiraj <nivalkardhiraj@gmail.com>
2021-09-15 12:32:26 +0530

    added checkauth in post ideas

--- api/routes/idea.js
+++ api/routes/idea.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const ideaController = require('../controllers/idea');
 const checkAuth = require('../middlewares/checkAuth');
 
-router.post('/part1', ideaController.postIdea1); 
-router.patch('/part2', ideaController.postIdea2); 
-router.patch('/part3', ideaController.postIdea3); 
+router.post('/part1', checkAuth, ideaController.postIdea1); 
+router.patch('/part2', checkAuth, ideaController.postIdea2); 
+router.patch('/part3', checkAuth, ideaController.postIdea3); 
 
 module.exports = router;
